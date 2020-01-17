06d9a1b96df08e5ebe305b4d7168a79d6af0cbbf
s.orlov <sergioorlove@gmail.com>
2020-01-17 20:42:34 +0300

    add project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LogoComponent } from './components/header/logo/logo.component';
+import { MenuComponent } from './components/header/menu/menu.component';
+import { IntroComponent } from './components/main-layout/intro/intro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LogoComponent,
+    MenuComponent,
+    IntroComponent,
   ],
   imports: [
     BrowserModule
